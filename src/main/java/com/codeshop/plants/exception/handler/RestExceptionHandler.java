2608a22980884f4766e5d6f4bccc97315627ec85
rol-x <pl.charlessummers@gmail.com>
2022-09-04 16:51:34 +0200

    Add custom response to request syntax errors

--- src/main/java/com/codeshop/plants/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/codeshop/plants/exception/handler/RestExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.codeshop.plants.exception.handler;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.codeshop.plants.exception.EntityNotFoundException;
@@ -22,6 +26,11 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIncompleteUserException(IncompleteUserException exception) {
         return new ErrorResponse(HttpStatus.BAD_REQUEST, exception);
+    }
+
+    @Override
+    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex, new ErrorResponse(HttpStatus.BAD_REQUEST, ex), headers, status, request);
     }
 
     @ExceptionHandler(Exception.class)
